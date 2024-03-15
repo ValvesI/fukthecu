c79554e27019b72b893970fa5a251b5597af30cf
ValvesI <131627287+ValvesI@users.noreply.github.com>
2024-03-15 11:53:34 -0300

    .

--- jogo.js
+++ jogo.js
@@ -1,7 +1,4 @@
-let numeroSecreto = 8;
-let numeroSecreto1 = "Oito";
-let numeroSecreto2 = "oito";
-let numeroSecreto3 = "OITO"
+let numeroSecreto = Math.random()*10;
 console.log(numeroSecreto);
 let chute = '';
 
@@ -9,7 +6,7 @@ while(chute == ''){
    chute = prompt('escolha um número de 0 ao 10');
 }
 
-if (numeroSecreto == chute || chute == numeroSecreto1 || chute == numeroSecreto2 || chute == numeroSecreto3){
+if (numeroSecreto == chute){
     alert('parabens lil nigg');
     console.log('parabens lil nigg');
 }
