33307f734f54166d394173361435bdafdd94e3ad
ValvesI <131627287+ValvesI@users.noreply.github.com>
2024-03-15 11:33:06 -0300

    .

--- jogo.js
+++ jogo.js
@@ -2,7 +2,9 @@ let numeroSecreto = 8;
 let chute = prompt('escolha um número de 0 ao 10');
 if (numeroSecreto == chute) {
     alert('parabens lil nigg');
+    console.log('parabens lil nigg');
 }
     else {
         alert('errou lixo');
+        console.log('errou lixo');
     }
