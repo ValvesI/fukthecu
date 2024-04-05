a28b6eba8a6b30cd5eb017dd17bf1b829d6b1861
ValvesI <131627287+ValvesI@users.noreply.github.com>
2024-04-05 12:09:09 -0300

    .

--- cont.js
+++ cont.js
@@ -4,5 +4,7 @@ const paula = document.querySelectorAll('.botaos');
 
 for(let sulfetodepotassiosomadocomasuaprimapodemacabaremumserioeventocatastroficoquepodetematarpoispodesertoxicokkk = 0; sulfetodepotassiosomadocomasuaprimapodemacabaremumserioeventocatastroficoquepodetematarpoispodesertoxicokkk < paula.length; sulfetodepotassiosomadocomasuaprimapodemacabaremumserioeventocatastroficoquepodetematarpoispodesertoxicokkk++){
     console.log(sulfetodepotassiosomadocomasuaprimapodemacabaremumserioeventocatastroficoquepodetematarpoispodesertoxicokkk);
-
+    paula[1].classList.add('ativo');
 }
+
+paula[sulfetodepotassiosomadocomasuaprimapodemacabaremumserioeventocatastroficoquepodetematarpoispodesertoxicokkk].classList.add('ativo');
