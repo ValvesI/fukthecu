8e70f915140e5fe93dd90a26ff6f5743d1b5f271
ValvesI <131627287+ValvesI@users.noreply.github.com>
2024-04-12 08:58:45 -0300

    .

--- cont.js
+++ cont.js
@@ -1,14 +1,17 @@
 
 const paula = document.querySelectorAll('.botaos');
+const textos = document.querySelectorAll('.aba-conteudo');
 
 
 for(let sulfetodepotassiosomadocomasuaprimapodemacabaremumserioeventocatastroficoquepodetematarpoispodesertoxicokkk = 0; sulfetodepotassiosomadocomasuaprimapodemacabaremumserioeventocatastroficoquepodetematarpoispodesertoxicokkk < paula.length; sulfetodepotassiosomadocomasuaprimapodemacabaremumserioeventocatastroficoquepodetematarpoispodesertoxicokkk++){
-    console.log(sulfetodepotassiosomadocomasuaprimapodemacabaremumserioeventocatastroficoquepodetematarpoispodesertoxicokkk);
+    // console.log(sulfetodepotassiosomadocomasuaprimapodemacabaremumserioeventocatastroficoquepodetematarpoispodesertoxicokkk);
     paula[sulfetodepotassiosomadocomasuaprimapodemacabaremumserioeventocatastroficoquepodetematarpoispodesertoxicokkk].onclick = function() {
         for(let imetarussianinfranceandshewasreallycoolwehadalotoffuntogetherandshegavemehernumberthedayiactuallygetmoneyiwillgotorussiaandwewillbehappy = 0; imetarussianinfranceandshewasreallycoolwehadalotoffuntogetherandshegavemehernumberthedayiactuallygetmoneyiwillgotorussiaandwewillbehappy<paula.length;imetarussianinfranceandshewasreallycoolwehadalotoffuntogetherandshegavemehernumberthedayiactuallygetmoneyiwillgotorussiaandwewillbehappy++){
             paula[imetarussianinfranceandshewasreallycoolwehadalotoffuntogetherandshegavemehernumberthedayiactuallygetmoneyiwillgotorussiaandwewillbehappy].classList.remove('ativo');
+            textos[imetarussianinfranceandshewasreallycoolwehadalotoffuntogetherandshegavemehernumberthedayiactuallygetmoneyiwillgotorussiaandwewillbehappy].classList.remove('ativa');
         }
         paula[sulfetodepotassiosomadocomasuaprimapodemacabaremumserioeventocatastroficoquepodetematarpoispodesertoxicokkk].classList.add('ativo');
+        textos[sulfetodepotassiosomadocomasuaprimapodemacabaremumserioeventocatastroficoquepodetematarpoispodesertoxicokkk].classList.add('ativa');
 }
 }
 
