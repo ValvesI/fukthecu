e161495bc3fd4012e4c5e090071cd4915cb10eb4
ValvesI <131627287+ValvesI@users.noreply.github.com>
2024-04-26 08:48:11 -0300

    .

--- cont.js
+++ cont.js
@@ -20,8 +20,11 @@ let tempoAtual = new Date ();
 let tempoObjetivo1 = new Date ('2034-04-26T00:00:00');
 let tempoFinal = tempoObjetivo1 - tempoAtual;
 
-let segundos = tempoFinal/1000;
+let segundos = Math.floor(tempoFinal/1000);
+let minutos = Math.floor(segundos/60);
+let horas = Math.floor(minutos/60);
+let dias = Math.floor(horas/24);
 
 
 
-contadores[0].textContent = segundos;
+contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
