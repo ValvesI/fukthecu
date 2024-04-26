d982c0d9235d09b9e6894e59f563cb81d83affdd
ValvesI <131627287+ValvesI@users.noreply.github.com>
2024-04-26 09:00:50 -0300

    .

--- cont.js
+++ cont.js
@@ -18,6 +18,10 @@ for(let sulfetodepotassiosomadocomasuaprimapodemacabaremumserioeventocatastrofic
 const contadores = document.querySelectorAll(".contador") ;
 let tempoAtual = new Date ();
 let tempoObjetivo1 = new Date ('2034-04-26T00:00:00');
+let tempoObjetivo2 = new Date ('2029-07-22T00:00:00');
+let tempoObjetivo3 = new Date ('2026-04-27T00:00:00');
+let tempoObjetivo4 = new Date ('2090-12-15T00:00:00');
+
 let tempoFinal = tempoObjetivo1 - tempoAtual;
 
 let segundos = Math.floor(tempoFinal/1000);
