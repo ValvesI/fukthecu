bb9b235673dd71779af0caf4fd7dbdc0ae381ea1
ValvesI <131627287+ValvesI@users.noreply.github.com>
2024-04-26 08:39:27 -0300

    .

--- cont.js
+++ cont.js
@@ -15,3 +15,9 @@ for(let sulfetodepotassiosomadocomasuaprimapodemacabaremumserioeventocatastrofic
 }
 }
 
+const contador = document.queryselectorAll(".contador") ;
+let tempoAtual = new Date ();
+let tempoObjetivo1 = new Date ('2034-04-26T00:00:00');
+let tempoFinal = tempoObjetivo1 - tempoAtual;
+
+contadores [0].textContent = tempoFinal;
